2f38bf5e96c49839a8ef3be938639bc58975f14c
DESKTOP-C9OI2CS\kimsi <kimsin4004@gmail.com>
2018-12-02 18:55:28 +0900

    fov는 화각 전체 각도다. 그래서 2로 나눠서 tan값을 구해야 맞다!

--- Engine/RRay.cpp
+++ Engine/RRay.cpp
@@ -4,22 +4,22 @@
 
 RRay::RRay(int screenX, int screenY, int screenWith, int screenHeight)
 {
-#define _XM_NO_INTRINSICS_
-	RCamera* camara = CameraManager->GetCurrentCamera();
+	RCamera* camera = CameraManager->GetCurrentCamera();
+	float nearPlaneToHalfScreenHeight = tan(camera->GetFov() / 2);
+	float nearPlaneToHalfScreenWidth = nearPlaneToHalfScreenHeight * camera->GetScreenAspect(); //aspect : w/h
 
-	float fovY = tan(camara->GetFov());
-	float fovX = fovY * camara->GetScreenAspect(); //aspect : w/h
-
-	float vx = (2.0f * screenX / screenWith - 1.0f) *  fovX;
-	float vy = (-2.0f * screenY / screenHeight + 1.0f) * fovY ;
+	float vx = (2.0f * screenX / screenWith - 1.0f) * nearPlaneToHalfScreenWidth;
+	float vy = (-2.0f * screenY / screenHeight + 1.0f) * nearPlaneToHalfScreenHeight;
 
 	XMVECTOR rayOrigin = XMVectorSet(0.0f, 0.0f, 0.0f, 1.0f);
 	XMVECTOR rayDir = XMVectorSet(vx, vy, 1.0f, 0.0f);
 
-	XMMATRIX V = camara->GetViewMatrix();
+	XMMATRIX V = camera->GetViewMatrix();    
+	XMMATRIX invView = XMMatrixInverse(&XMMatrixDeterminant(V), V);
 
-	XMVECTOR originInWorldCoord = XMVector3TransformCoord(rayOrigin, V);
-	XMVECTOR dirInWorldCoord = XMVector3TransformNormal(rayDir, V);
+
+	XMVECTOR originInWorldCoord = XMVector3TransformCoord(rayOrigin, invView);
+	XMVECTOR dirInWorldCoord = XMVector3TransformNormal(rayDir, invView);
 
 	if (screenX == screenWith / 2 && screenY == screenHeight / 2)
 		printf("asdf");
